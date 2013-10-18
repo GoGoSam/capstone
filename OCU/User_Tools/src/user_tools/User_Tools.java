f3302ab3d70bcb5712b57df549e119e0321873e7
Joe Robinson <robinson.jo@husky.neu.edu>
2013-10-18 09:42:36 -0400

    copied GUI files for the Main Menu UI and the arrow-directionals implemented with key listeners

---
+++ OCU/User_Tools/src/user_tools/User_Tools.java
@@ -0,0 +1,18 @@
+package user_tools;
+
+/**
+ *
+ * @author Sam Coe and Joe Robinson
+ */
+public class User_Tools {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //XBox360_DirectionDisplay ff = null;
+        MobileDirectionDisplay mdd = null;
+        mdd = new MobileDirectionDisplay();
+        mdd.setVisible(true);
+    }
+}
