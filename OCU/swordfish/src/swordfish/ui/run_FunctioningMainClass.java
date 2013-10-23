4818408f37dd0aa6b973f1137d903bc5dee5a912
Joe Robinson <robinson.jo@husky.neu.edu>
2013-10-23 17:08:37 -0400

    added function to run MainMenu made for development

---
+++ OCU/swordfish/src/swordfish/ui/run_FunctioningMainClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swordfish.ui;
+
+/**
+ *
+ * @author jrob
+ */
+public class run_FunctioningMainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        FunctioningMainMenu fmm = new FunctioningMainMenu();
+        fmm.setVisible(true);
+        //FunctioningMainMenu.se
+    }
+}
