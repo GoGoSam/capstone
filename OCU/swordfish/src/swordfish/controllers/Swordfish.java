036e640fa24fde32f24bb5568632fd536ecb867d
Sam Coe <sam.coe@me.com>
2013-12-23 16:53:02 -0600

    Defined general architecture for swordfish and created TCPClient, VideoStreamer, and RobotController

--- OCU/swordfish/src/swordfish/controllers/Swordfish.java
+++ OCU/swordfish/src/swordfish/controllers/Swordfish.java
@@ -2,6 +2,8 @@ package swordfish.controllers;
 
 import swordfish.views.MobileDirectionDisplayKeyboard;
 
+import javax.swing.*;
+
 /**
  *
  * @author Sam Coe and Joe Robinson
@@ -11,14 +13,27 @@ import swordfish.views.MobileDirectionDisplayKeyboard;
  * Revisions: 1) December 17, 2013, JR - Updated according to external class
  * filename. MobileDirectionDisplay -> MobileDirectionDisplayKeyboard
  */
+
+/*
+ * Swordfish Tasks:
+ *  Initialize UI
+ *  Initialize RobotController
+ *  Initialize VideoStreaming
+ */
+
 public class Swordfish {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
-        MobileDirectionDisplayKeyboard mddk = new MobileDirectionDisplayKeyboard();
-        mddk.setVisible(true);
+        String addr = "192.168.1.1";
+        int port = 5000;
+        JFrame ui = new JFrame();
+        //JFrame ui = new UI();
+        //RobotController rc = new RobotController();
+        //rc.connect(addr, port, ui);
+        VideoStreamer vs = new VideoStreamer();
+        vs.connect(addr, port, ui);
     }
 }
